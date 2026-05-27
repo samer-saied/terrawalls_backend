adb49c8e5c59b1355a754aee68df7440cb5ea3f3
Samer Saied <46113474+samer-saied@users.noreply.github.com>
2026-05-27 07:23:30 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const userData = {
   first_name: "Terra",
   last_name: "Walls",
   twitter_username: null,
-  portfolio_url: "https://samer-saied.vercel.app/",
+  portfolio_url: "https://www.samersaied.site/",
   bio: null,
   location: "Egypt",
   instagram_username: null,
